3cf189dcd639e087f0a3058e17372963a5333622
Seleem Alabi <117839317+leem-dev@users.noreply.github.com>
2023-12-28 18:25:01 +0100

    Add files via upload

---
+++ src/images/images.js
@@ -0,0 +1,53 @@
+import foggyForest from "../assets/foggyForest.jpeg";
+import greenGrass from "../assets/greenGrass.jpeg";
+import greenStairs from "../assets/greenStairs.jpeg";
+import iceLand from "../assets/iceLand.jpeg";
+import middleRock from "../assets/middleRock.jpeg";
+import sandPour from "../assets/sandPour.jpeg";
+import waterView from "../assets/waterView.jpeg";
+
+export const Images = [
+  {
+    id: 1,
+    src: foggyForest,
+    title: "Foggy Forest",
+    description: "foggy forest",
+  },
+  {
+    id: 2,
+    src: greenGrass,
+    title: "Green Grass",
+    description: "green grass",
+  },
+  {
+    id: 3,
+    src: greenStairs,
+    title: "Green Stairs",
+    description: "green stairs",
+    url: "https://www.pexels.com/photo/scenic-view-of-rice-paddy-247599/",
+  },
+  {
+    id: 4,
+    src: iceLand,
+    title: "Ice Land",
+    description: "ice land",
+  },
+  {
+    id: 5,
+    src: middleRock,
+    title: "Middle sea Rock",
+    description: "middle rock",
+  },
+  {
+    id: 6,
+    src: sandPour,
+    title: "Sand Pour",
+    description: "sand pour",
+  },
+  {
+    id: 7,
+    src: waterView,
+    title: "Water View",
+    description: "water view",
+  },
+];
